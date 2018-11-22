5582b477a70ee60ad2164ae2f5508dd90ca5f107
Golam Rabbani <rabbycse171@gmail.com>
2018-11-22 19:47:15 +0600

    Numerical Methods

---
+++ prog-1.c
@@ -0,0 +1,143 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include<math.h>
+#define e 0.001
+#define f(x) cos(x)-3*x+2
+#define g(x) (cos(x)+2)/3
+#define df(x) -sin(x)-3
+
+
+int main()
+{
+    system("Color 3F");
+
+    int choice;
+    float a,b,x0=0,x1=0,f0,f1,fn,fr,Xn,Xr,dfn;
+
+    int n = 0, r = 0, k = 0;
+
+    printf("         Welcome         \n");
+    printf("   Solution of Equation");
+    printf("\n----------------------------\n");
+    while(1)
+    {
+        printf("\n\n  ************************");
+        printf("\n @________MAIN MENU_______@\n");
+        printf(" # 1. Bisection           #\n");
+        printf(" # 2. Iteration           #\n");
+        printf(" # 3. Newton-Raphson's    #\n");
+        printf(" # 4. Exit                #\n");
+        printf(" **************************\n");
+        printf(" Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch(choice)
+        {
+        case 1:
+            system("cls");
+            system("Color 2F");
+
+            printf("    @Solution of Bisection Method@\n");
+            printf("_________________________________________\n");
+            printf("\n Let,f(x) = cos(x)-3*x+2\n");
+
+            printf(" Enter the value of a(0) and b(1): ");
+            scanf("%f %f",&a,&b);
+
+            do
+            {
+                f0=f(a);
+                f1=f(b);
+
+                Xr=(a+b)/2;
+
+                fn=f(Xr);
+
+                if(a*fn<0)
+                {
+                    b=Xr;
+                }
+                else         /// (b*fn<0)
+                {
+                    a=Xr;
+                }
+                n++;
+                printf("\n No.of %d required root = %f",n,Xr);
+
+
+            }
+            while(fabs(fn)>e);  // fabs is return the absolute value
+            printf("\n\n So required solution root is %.3f\n",Xr);
+            break;
+
+        case 2:
+            system("cls");
+            system("Color 6F");
+
+            printf("    @Solution of Iteration Method@\n");
+            printf("_________________________________________\n");
+            printf("\n Let,f(x) = cos(x)-3*x+2\n");
+            printf("     g(x) = (cos(x)+2)/3");
+
+
+            printf("\n Enter the initial value(1): ");
+            scanf("%f",&x0);
+            do
+            {
+                if(g(x0)<1)
+                {
+                    x1=g(x0);
+                    Xr=(x1-x0)/x1;
+                    x0=x1;
+                    r++;
+                    printf("\n No.of X%d required root = %f",r,x0);
+                }
+                else
+                {
+                    printf("\n Your input is invalid!!!!");
+                }
+            }
+            while(fabs(Xr)>e);
+            printf("\n\n So required solution root is %.3f\n",x0);
+            break;
+
+        case 3:
+            system("cls");
+            system("Color 3F");
+
+            printf("    @Solution of Newton-Raphson Method@\n");
+            printf("___________________________________________\n");
+            printf("\n Let,f(x) = cos(x)-3*x+2\n");
+            printf("    df(x) = -sin(x)-3");
+
+            printf("\n Enter the initial value(1) : ");
+            scanf("%f",&Xn);
+
+            do
+            {
+                fn = f(Xn);
+                fr = f(Xr);
+                dfn = df(Xn);
+                Xr = Xn-(fn/dfn);
+                Xn = Xr;
+                k++;
+                printf("\n No.of X%d required root = %f",k,Xr);
+
+            }
+            while(fabs(fr)>e);
+            printf("\n\n So required solution root is %.3f\n",Xr);
+            break;
+
+        case 4:
+            printf("\n\tTHANK YOU FOR WATCHING ^_^ \n");
+            return 0;
+
+        default:
+            printf("Enter correct number e.g 1 - 4\n");
+            break;
+
+        }
+    }
+
+}
